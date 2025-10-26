4a30ff4239a3f0b108fe820930a23704202beb05
italo1033 <italogammy1033@gmail.com>
2025-10-26 18:39:32 -0300

    Refactor main.py to remove CRUD operations and integrate item routes

--- app/main.py
+++ app/main.py
@@ -1,41 +1,16 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-import models, schemas, crud
-from database import engine, Base, get_db
+from fastapi import FastAPI
+from core.database import Base, engine
+from api.routes.item import router as items
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="FastAPI CRUD PostgreSQL")
 
+
 @app.get("/")
 def root():
     return {"message": "API funcionando com PostgreSQL!"}
 
-@app.post("/items/", response_model=schemas.ItemResponse)
-def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
-    return crud.create_item(db, item)
 
-@app.get("/items/", response_model=list[schemas.ItemResponse])
-def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    return crud.get_items(db, skip=skip, limit=limit)
-
-@app.get("/items/{item_id}", response_model=schemas.ItemResponse)
-def read_item(item_id: int, db: Session = Depends(get_db)):
-    db_item = crud.get_item(db, item_id)
-    if not db_item:
-        raise HTTPException(status_code=404, detail="Item não encontrado")
-    return db_item
-
-@app.put("/items/{item_id}", response_model=schemas.ItemResponse)
-def update_item(item_id: int, item: schemas.ItemUpdate, db: Session = Depends(get_db)):
-    db_item = crud.update_item(db, item_id, item)
-    if not db_item:
-        raise HTTPException(status_code=404, detail="Item não encontrado")
-    return db_item
-
-@app.delete("/items/{item_id}", response_model=schemas.ItemResponse)
-def delete_item(item_id: int, db: Session = Depends(get_db)):
-    db_item = crud.delete_item(db, item_id)
-    if not db_item:
-        raise HTTPException(status_code=404, detail="Item não encontrado")
-    return db_item
+app.include_router(items)
